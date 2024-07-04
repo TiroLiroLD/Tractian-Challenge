d94a0b5dd60f3597ac290f0adf1ffcff737035fd
Leone Crespo Daher de Souza <E944374@iberdrola.com>
2024-07-04 06:51:39 -0300

    fix text overflow with ticker

--- tractian_challenge/lib/presentation/widgets/collapsible_widget.dart
+++ tractian_challenge/lib/presentation/widgets/collapsible_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 class CollapsibleWidget extends StatefulWidget {
   final String title;
@@ -21,13 +22,44 @@ class CollapsibleWidget extends StatefulWidget {
   _CollapsibleWidgetState createState() => _CollapsibleWidgetState();
 }
 
-class _CollapsibleWidgetState extends State<CollapsibleWidget> {
+class _CollapsibleWidgetState extends State<CollapsibleWidget> with SingleTickerProviderStateMixin {
   late bool _isExpanded;
+  late final ScrollController _scrollController;
+  late Ticker _ticker;
+  bool _scrollingForward = true;
+  int _pauseCounter = 0;
 
   @override
   void initState() {
     super.initState();
     _isExpanded = widget.isExpanded;
+    _scrollController = ScrollController();
+
+    _ticker = createTicker((elapsed) {
+      const int pauseDuration = 100; // Number of ticks to pause at the end
+      const double scrollSpeed = 0.5; // Scroll speed
+
+      if (_scrollingForward) {
+        if (_scrollController.position.pixels >= _scrollController.position.maxScrollExtent) {
+          _pauseCounter++;
+          if (_pauseCounter >= pauseDuration) {
+            _scrollingForward = false;
+            _pauseCounter = 0;
+            _scrollController.jumpTo(_scrollController.position.minScrollExtent);
+          }
+        } else {
+          _scrollController.jumpTo(_scrollController.position.pixels + scrollSpeed);
+        }
+      } else {
+        _pauseCounter++;
+        if (_pauseCounter >= pauseDuration) {
+          _scrollingForward = true;
+          _pauseCounter = 0;
+        }
+      }
+    });
+
+    WidgetsBinding.instance.addPostFrameCallback((_) => _ticker.start());
   }
 
   @override
@@ -38,6 +70,13 @@ class _CollapsibleWidgetState extends State<CollapsibleWidget> {
         _isExpanded = widget.isExpanded;
       });
     }
+  }
+
+  @override
+  void dispose() {
+    _ticker.dispose();
+    _scrollController.dispose();
+    super.dispose();
   }
 
   @override
@@ -55,10 +94,10 @@ class _CollapsibleWidgetState extends State<CollapsibleWidget> {
                 onPressed: widget.disableCollapse
                     ? null
                     : () {
-                        setState(() {
-                          _isExpanded = !_isExpanded;
-                        });
-                      },
+                  setState(() {
+                    _isExpanded = !_isExpanded;
+                  });
+                },
               ),
             if (widget.children.isEmpty)
               const SizedBox(
@@ -72,29 +111,33 @@ class _CollapsibleWidgetState extends State<CollapsibleWidget> {
             ),
             const SizedBox(width: 8),
             Expanded(
-              child: Row(
-                children: [
-                  Text(
-                    widget.title,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  if (widget.status != null)
-                    Padding(
-                      padding: const EdgeInsets.only(left: 8.0),
-                      child: Icon(
-                        widget.status == 'alert'
-                            ? Icons.circle
-                            : Icons.bolt_rounded,
-                        color: widget.status == 'alert'
-                            ? Colors.red
-                            : Colors.green,
-                        size: widget.status == 'alert' ? 12 : null,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                controller: _scrollController,
+                child: Row(
+                  children: [
+                    Text(
+                      widget.title,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                ],
+                    if (widget.status != null)
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: Icon(
+                          widget.status == 'alert'
+                              ? Icons.circle
+                              : Icons.bolt_rounded,
+                          color: widget.status == 'alert'
+                              ? Colors.red
+                              : Colors.green,
+                          size: widget.status == 'alert' ? 12 : null,
+                        ),
+                      ),
+                  ],
+                ),
               ),
             ),
           ],
