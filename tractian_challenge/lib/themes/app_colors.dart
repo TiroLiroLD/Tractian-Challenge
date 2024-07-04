6359aa8c22f9f83dc2d4457f73b922030507815c
Leone Crespo Daher de Souza <E944374@iberdrola.com>
2024-07-04 01:51:03 -0300

    update app theme colors

--- tractian_challenge/lib/themes/app_colors.dart
+++ tractian_challenge/lib/themes/app_colors.dart
@@ -3,12 +3,20 @@ import 'package:flutter/material.dart';
 class AppColors {
   // Default Theme Colors
   static const Color buttonBackground = Color(0xFF2188FF);
+  static const Color buttonText = Color(0xFFFFFFFF);
+  static const Color buttonOffBackground = Color(0xFFFFFFFF);
+  static const Color buttonOffBorder = Color(0xFFD8DFE6);
+  static const Color buttonOffText = Color(0xFF77818C);
+
   static const Color headerBackground = Color(0xFF17192D);
   static const Color headerText = Color(0xFFFFFFFF);
-  static const Color buttonText = Color(0xFFFFFFFF);
+
   static const Color bodyBackground = Color(0xFFFFFFFF);
+
   static const Color searchBoxBackground = Color(0xFFEAEFF3);
   static const Color searchBoxText = Color(0xFF8E98A3);
+  static const Color searchBoxIconColor = Color(0xFF8E98A3);
+
   static const Color bodyText2 = Color(0xFF77818C);
   static const Color bodyText1 = Color(0xFF17192D);
 
