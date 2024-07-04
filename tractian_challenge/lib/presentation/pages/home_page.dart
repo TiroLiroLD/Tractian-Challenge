6184c0151a31d65e45f30ca33663ec0cd729c43e
Leone Crespo Daher de Souza <E944374@iberdrola.com>
2024-07-04 01:56:49 -0300

    refactor split Home/Unit pages
    load data from DataService

--- tractian_challenge/lib/presentation/pages/home_page.dart
+++ tractian_challenge/lib/presentation/pages/home_page.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:gap/gap.dart';
+import 'package:tractian_challenge/data/services/data_service.dart';
+import 'package:tractian_challenge/presentation/pages/unit_page.dart';
 import 'package:tractian_challenge/themes/app_colors.dart';
+import 'package:tractian_challenge/data/models/asset.dart';
+import 'package:tractian_challenge/data/models/location.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final DataService _dataService = DataService();
+  late Future<List<Map<String, dynamic>>> _unitsFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _unitsFuture = _dataService.fetchUnits();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,28 +32,46 @@ class HomePage extends StatelessWidget {
           color: Theme.of(context).appBarTheme.iconTheme?.color,
         ),
         backgroundColor: Theme.of(context).primaryColor,
-        centerTitle: true, // Center the title to match the design
+        centerTitle: true,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            _buildUnitButton(context, 'Jaguar Unit'),
-            const Gap(16),
-            _buildUnitButton(context, 'Tobias Unit'),
-            const Gap(16),
-            _buildUnitButton(context, 'Apex Unit'),
-          ],
+        child: FutureBuilder<List<Map<String, dynamic>>>(
+          future: _unitsFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return const Center(child: Text('No units available'));
+            } else {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: snapshot.data!.map((unit) {
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 16.0),
+                    child: _buildUnitButton(
+                      context,
+                      unit['name'],
+                      unit['locations'],
+                      unit['assets'],
+                    ),
+                  );
+                }).toList(),
+              );
+            }
+          },
         ),
       ),
     );
   }
 
-  Widget _buildUnitButton(BuildContext context, String unitName) {
+  Widget _buildUnitButton(
+      BuildContext context, String unitName, List<Location> locations, List<Asset> assets) {
     return Container(
-      width: 317, // Fixed width
-      height: 76, // Fixed height
+      width: 317,
+      height: 76,
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
           backgroundColor: AppColors.buttonBackground,
@@ -46,11 +81,14 @@ class HomePage extends StatelessWidget {
           ),
         ),
         onPressed: () {
-          // Navigate to the corresponding unit's page
           Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (context) => UnitPage(unitName: unitName),
+              builder: (context) => UnitPage(
+                unitName: unitName,
+                locations: locations,
+                assets: assets,
+              ),
             ),
           );
         },
@@ -58,28 +96,6 @@ class HomePage extends StatelessWidget {
           unitName,
           style: const TextStyle(color: AppColors.buttonText),
         ),
-      ),
-    );
-  }
-}
-
-class UnitPage extends StatelessWidget {
-  final String unitName;
-
-  const UnitPage({required this.unitName});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          unitName,
-          style: const TextStyle(color: AppColors.headerText),
-        ),
-        backgroundColor: AppColors.headerBackground,
-      ),
-      body: Center(
-        child: Text('Details for $unitName'),
       ),
     );
   }
